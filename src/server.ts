ecdf92e7159111cd3a60ca76c1560da0757245df
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-06-21 16:56:55 -0300

    First POST route

--- src/server.ts
+++ src/server.ts
@@ -7,4 +7,8 @@ app.get("/teste", (request, response) => {
     return response.send("Olá de novo");
 });
 
+app.post("/teste-post", (request, response) => {
+    return response.send("Olá, mas no post");
+});
+
 app.listen(3000, () => console.log("Rodou o server e atualizou"));
