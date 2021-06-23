567aba999e031c95635a0fab4ff7d9b4e9c8ff41
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-06-23 16:53:19 -0300

    Creating database setting and migrations

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,9 @@
+import "reflect-metadata"; 
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-
-app.get("/teste", (request, response) => {
-    return response.send("Olá de novo");
-});
-
-app.post("/teste-post", (request, response) => {
-    return response.send("Olá, mas no post");
-});
+ 
 
 app.listen(3000, () => console.log("Rodou o server e atualizou"));
