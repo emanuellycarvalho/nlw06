17f769a814ee851ac674c835cadb50e11799ed71
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-06-23 20:18:50 -0300

    Handling async error on server

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "./database";
 import "reflect-metadata"; 
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-error";
 
 import { router } from "./routes";
 
@@ -9,4 +10,19 @@ const app = express();
  
 app.use(express.json);
 app.use(router);
+
+//middleware
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error"
+    })
+});
+
 app.listen(3000, () => console.log("Rodou o server e atualizou"));
