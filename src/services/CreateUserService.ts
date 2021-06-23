373c5f7d0f0fe0c5c868ccffd82e598f98fdfdd3
Emanuelly Carvalho <emanuellyvcarv@gmail.com>
2021-06-23 17:28:35 -0300

    Creating a repository and a service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,35 @@
+import { UserRepositories } from "../repositories/UserRepositories"
+
+interface IUserRequest {
+    name: string;
+    email: string;
+    admin?: boolean
+}
+
+class CreateUserService{
+
+    async execute({ name, email, admin } : IUserRequest){
+        const userRepository = new UserRepositories();
+
+        if (!email){
+            throw new Error("Please, insert a valid e-mail");
+        }
+        const userAlreadyExists = await userRepository.findOne({
+            email
+        });
+
+        if(userAlreadyExists){
+            throw new Error("Another user is already using that e-mail");
+        }
+
+        const user = userRepository.create({
+            name,
+            email, 
+            admin
+        });
+
+        await userRepository.save(user);
+    }
+}
+
+export { CreateUserService }
